039028dd422e8c9491980db8602e77eee538519b
Jan Jörg <jan.joerg.jj@gmail.com>
2024-11-12 12:29:24 +0100

    ux

--- src/routes/twilio-test/+page.svelte
+++ src/routes/twilio-test/+page.svelte
@@ -11,101 +11,104 @@
 </script>
 
 <div class="space-y-5">
-  <h2>Send Message</h2>
-  <form
-    action="?/sendMessage"
-    class=""
-    method="POST"
-    use:enhance={({ formElement, formData, action, cancel, submitter }) => {
-      // `formElement` is this `<form>` element
-      // `formData` is its `FormData` object that's about to be submitted
-      // `action` is the URL to which the form is posted
-      // calling `cancel()` will prevent the submission
-      // `submitter` is the `HTMLElement` that caused the form to be submitted
+  {#if numberId}
+    <h2>Whatsapp Nachricht versenden</h2>
 
-      return async ({ result, update }) => {
-        if (result.type === "success") {
-          showSuccess = true;
-          formElement.reset();
-          invalidateAll();
-        } else {
-          showError = true;
-          invalidateAll();
-          formElement.reset();
-        }
-        setTimeout(() => {
-          showSuccess = false;
-          showError = false;
-        }, 3500);
-        // `result` is an `ActionResult` object
-        // `update` is a function which triggers the default logic that would be triggered if this callback wasn't set
-      };
-    }}
-  >
-    <div class="space-y-3">
-      <div class="md:flex md:space-x-2">
-        <div class="w-full">
-          <label for="text">Nachricht</label>
-          <input type="text" name="text" />
+    <form
+      action="?/sendMessage"
+      class=""
+      method="POST"
+      use:enhance={({ formElement, formData, action, cancel, submitter }) => {
+        // `formElement` is this `<form>` element
+        // `formData` is its `FormData` object that's about to be submitted
+        // `action` is the URL to which the form is posted
+        // calling `cancel()` will prevent the submission
+        // `submitter` is the `HTMLElement` that caused the form to be submitted
+
+        return async ({ result, update }) => {
+          if (result.type === "success") {
+            showSuccess = true;
+            formElement.reset();
+            invalidateAll();
+          } else {
+            showError = true;
+            invalidateAll();
+            formElement.reset();
+          }
+          setTimeout(() => {
+            showSuccess = false;
+            showError = false;
+          }, 3500);
+          // `result` is an `ActionResult` object
+          // `update` is a function which triggers the default logic that would be triggered if this callback wasn't set
+        };
+      }}
+    >
+      <div class="space-y-3">
+        <div class="md:flex md:space-x-2">
+          <div class="w-full">
+            <label for="text">Nachricht</label>
+            <input type="text" name="text" />
+          </div>
+          <div class="w-full">
+            <label for="text">Id</label>
+            <input type="text" name="number" bind:value={numberId} />
+          </div>
         </div>
-        <div class="w-full">
-          <label for="text">NummerId</label>
-          <input type="text" name="number" />
-        </div>
+        <br />
+        <button
+          class="px-3 py-2 bg-blue-300 rounded-md shadow-md"
+          type="submit"
+        >
+          senden
+        </button>
       </div>
-      <br />
-      <button class="px-3 py-2 bg-blue-300 rounded-md shadow-md" type="submit">
-        senden
-      </button>
-    </div>
-  </form>
+    </form>
+  {/if}
 
-  <br />
-  <br />
-  <br />
-  <h2>GetNumberId</h2>
+  {#if !numberId}
+    <h2>Gib deine Handynummer ein</h2>
 
-  <form
-    action="?/getChatId"
-    class=""
-    method="POST"
-    use:enhance={({ formElement, formData, action, cancel, submitter }) => {
-      // `formElement` is this `<form>` element
-      // `formData` is its `FormData` object that's about to be submitted
-      // `action` is the URL to which the form is posted
-      // calling `cancel()` will prevent the submission
-      // `submitter` is the `HTMLElement` that caused the form to be submitted
+    <form
+      action="?/getChatId"
+      class=""
+      method="POST"
+      use:enhance={({ formElement, formData, action, cancel, submitter }) => {
+        // `formElement` is this `<form>` element
+        // `formData` is its `FormData` object that's about to be submitted
+        // `action` is the URL to which the form is posted
+        // calling `cancel()` will prevent the submission
+        // `submitter` is the `HTMLElement` that caused the form to be submitted
 
-      return async ({ result, update }) => {
-        if (result.type === "success") {
-          numberId = String(await result.data.data);
-          formElement.reset();
+        return async ({ result, update }) => {
+          if (result.type === "success") {
+            numberId = String(await result.data.data);
+            formElement.reset();
 
-          invalidateAll();
-        } else {
-          invalidateAll();
-          formElement.reset();
-        }
-        // `result` is an `ActionResult` object
-        // `update` is a function which triggers the default logic that would be triggered if this callback wasn't set
-      };
-    }}
-  >
-    <div class="space-y-3">
-      <label for="number">Nummer</label>
-      <input type="tel" name="number" autocomplete="tel" />
-      <br />
-      <button class="px-3 py-2 bg-blue-300 rounded-md shadow-md" type="submit">
-        abfragen
-      </button>
-    </div>
-  </form>
+            invalidateAll();
+          } else {
+            invalidateAll();
+            formElement.reset();
+          }
+          // `result` is an `ActionResult` object
+          // `update` is a function which triggers the default logic that would be triggered if this callback wasn't set
+        };
+      }}
+    >
+      <div class="space-y-3">
+        <label for="number">Nummer</label>
+        <input type="tel" name="number" autocomplete="tel" />
+        <br />
+        <button
+          class="px-3 py-2 bg-blue-300 rounded-md shadow-md"
+          type="submit"
+        >
+          absenden
+        </button>
+      </div>
+    </form>
+  {/if}
 </div>
-{#if numberId}
-  <div class="">
-    {numberId}
-  </div>
-{/if}
 
 {#if showSuccess || showError}
   <div
