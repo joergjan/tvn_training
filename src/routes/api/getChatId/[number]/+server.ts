a084ac76e50c1cac1eec47953c8615a99348b9d2
Jan Jörg <jan.joerg.jj@gmail.com>
2024-11-12 12:20:07 +0100

    get chat id bugfix

--- src/routes/api/getChatId/[number]/+server.ts
+++ src/routes/api/getChatId/[number]/+server.ts
@@ -1,6 +1,22 @@
 import { fail, json } from "@sveltejs/kit";
 import { WAAPI_TOKEN, WAAPI_CLIENT, API_KEY } from "$env/static/private";
 
+function formatSwissPhoneNumber(phone: string) {
+  // Remove all non-digit characters
+  phone = phone.replace(/\D/g, "");
+
+  // Check and replace country code if it starts with 00 or +
+  if (phone.startsWith("0041")) {
+    phone = phone.replace(/^0041/, "41");
+  } else if (phone.startsWith("41")) {
+    // Already starts with the correct country code
+  } else if (phone.startsWith("0")) {
+    phone = "41" + phone.slice(1);
+  }
+
+  return phone;
+}
+
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ params }) {
   /*
@@ -12,11 +28,11 @@ export async function GET({ params }) {
   }
   */
 
-  const number = params.number;
+  const number = formatSwissPhoneNumber(params.number);
 
   if (number) {
     try {
-      const url = `https://waapi.app/api/v1/instances/id/${WAAPI_CLIENT}/action/get-number-id`;
+      const url = `https://waapi.app/api/v1/instances/${WAAPI_CLIENT}/client/action/get-number-id`;
       const options = {
         method: "POST",
         headers: {
@@ -30,6 +46,7 @@ export async function GET({ params }) {
       };
       const response = await fetch(url, options);
       const data = await response.json();
+      console.log(data);
       const numberId: string = data.data.data.numberId._serialized;
 
       if (response.ok) {
